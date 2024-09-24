5c402d278891f42b75e0d9db53a3d81ebc4ead19
Whale <whale082096@gmail.com>
2024-09-24 11:03:00 -0400

    developed coding to download data from opendatatoronto

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,26 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Open Data Toronto.
+# Author: Jing Liang
+# Date: 23 September 2024 
+# Contact: jess.liang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: installation and loading of R package "opendatatoronto".
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
-
+# Conduct a search for available data files that are related to the "Transport Bus Initiative Usage"
+data_packages <- search_packages("Transport Bus Initiative Usage")
+# Retreive all the resources available within the data files
+data_resources <- data_packages %>%
+  list_package_resources()
+# Retrieve the second resource (the .csv file)  and save it as a dataframe
+raw_data <- data_resources[2,] %>%
+  get_resource()
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(raw_data, "data/raw_data/ttc_bus_rawdata.csv") 
 
-         
